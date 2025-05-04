05b398f21c80769c812fe98a6dc7b04333f3061a
haru <h.harupee@gmail.com>
2025-05-05 08:42:20 +0900

    cs1

---
+++ js/character-checker.js
@@ -0,0 +1,276 @@
+const grid = document.getElementById("characterGrid");
+const cstats = document.getElementById("cstats");
+
+characters.forEach((char, index) => {
+    const div = document.createElement("div");
+    div.className = "character";
+    div.dataset.index = index;
+    div.innerHTML = `
+    <img src="${char.darkFile}" alt="${char.name}">
+    <div class="character-name">${char.name}</div>
+  `;
+    div.addEventListener("click", () => toggleCharacter(div, index));
+    grid.appendChild(div);
+});
+
+function toggleCharacter(div, index) {
+    div.classList.toggle("owned");
+    const img = div.querySelector("img");
+    img.src = div.classList.contains("owned") ? characters[index].file : characters[index].darkFile;
+    updateStats();
+}
+
+function updateStats() {
+    const cstats = document.getElementById("cstats");
+    const sakiStats = document.getElementById("sakiStats");
+    const temariStats = document.getElementById("temariStats");
+    const kotoneStats = document.getElementById("kotoneStats");
+    const maoStats = document.getElementById("maoStats");
+    const ririyaStats = document.getElementById("ririyaStats");
+    const tinaStats = document.getElementById("tinaStats");
+    const sumikaStats = document.getElementById("sumikaStats");
+    const hiroStats = document.getElementById("hiroStats");
+    const senaStats = document.getElementById("senaStats");
+    const umeStats = document.getElementById("umeStats");
+    const rinamiStats = document.getElementById("rinamiStats");
+
+
+
+
+
+    const owned = document.querySelectorAll(".character.owned").length;
+    const total = characters.length;
+    const percent = ((owned / total) * 100).toFixed(1);
+
+    cstats.innerHTML = `全<span class="red">${total}</span>種中<span class="red">${owned}</span>種（ 所持率<span class="red">${percent}%</span> ）`;
+
+    // 咲季のカードだけを対象にカウント
+    let sakiTotal = 0;
+    let sakiOwned = 0;
+
+    characters.forEach((char, i) => {
+        if (char.chara === "咲季") {
+            sakiTotal++;
+            const div = document.querySelector(`.character[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                sakiOwned++;
+            }
+        }
+    });
+
+    const sakiPercent = sakiTotal > 0 ? ((sakiOwned / sakiTotal) * 100).toFixed(1) : "0.0";
+
+    sakiStats.innerHTML = `咲季:<span class="red">${sakiTotal}</span>種中<span class="red">${sakiOwned}</span>種（ 所持率<span class="red">${sakiPercent}%</span> ）`;
+
+    // 手毬のカードだけを対象にカウント
+    let temariTotal = 0;
+    let temariOwned = 0;
+
+    characters.forEach((char, i) => {
+        if (char.chara === "手毬") {
+            temariTotal++;
+            const div = document.querySelector(`.character[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                temariOwned++;
+            }
+        }
+    });
+
+    const temariPercent = temariTotal > 0 ? ((temariOwned / temariTotal) * 100).toFixed(1) : "0.0";
+
+    temariStats.innerHTML = `手毬:<span class="red">${temariTotal}</span>種中<span class="red">${temariOwned}</span>種（ 所持率<span class="red">${temariPercent}%</span> ）`;
+
+    // ことねのカードだけを対象にカウント
+    let kotoneTotal = 0;
+    let kotoneOwned = 0;
+
+    characters.forEach((char, i) => {
+        if (char.chara === "ことね") {
+            kotoneTotal++;
+            const div = document.querySelector(`.character[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                kotoneOwned++;
+            }
+        }
+    });
+
+    const kotonePercent = kotoneTotal > 0 ? ((kotoneOwned / kotoneTotal) * 100).toFixed(1) : "0.0";
+
+    kotoneStats.innerHTML = `ことね:<span class="red">${kotoneTotal}</span>種中<span class="red">${kotoneOwned}</span>種（ 所持率<span class="red">${kotonePercent}%</span> ）`;
+
+    // 麻央のカードだけを対象にカウント
+    let maoTotal = 0;
+    let maoOwned = 0;
+
+    characters.forEach((char, i) => {
+        if (char.chara === "麻央") {
+            maoTotal++;
+            const div = document.querySelector(`.character[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                maoOwned++;
+            }
+        }
+    });
+
+    const maoPercent = maoTotal > 0 ? ((maoOwned / maoTotal) * 100).toFixed(1) : "0.0";
+
+    maoStats.innerHTML = `麻央:<span class="red">${maoTotal}</span>種中<span class="red">${maoOwned}</span>種（ 所持率<span class="red">${maoPercent}%</span> ）`;
+
+    // リーリヤのカードだけを対象にカウント
+    let ririyaTotal = 0;
+    let ririyaOwned = 0;
+
+    characters.forEach((char, i) => {
+        if (char.chara === "リーリヤ") {
+            ririyaTotal++;
+            const div = document.querySelector(`.character[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                ririyaOwned++;
+            }
+        }
+    });
+
+    const ririyaPercent = ririyaTotal > 0 ? ((ririyaOwned / ririyaTotal) * 100).toFixed(1) : "0.0";
+
+    ririyaStats.innerHTML = `リーリヤ:<span class="red">${ririyaTotal}</span>種中<span class="red">${ririyaOwned}</span>種（ 所持率<span class="red">${ririyaPercent}%</span> ）`;
+
+    // 千奈のカードだけを対象にカウント
+    let tinaTotal = 0;
+    let tinaOwned = 0;
+
+    characters.forEach((char, i) => {
+        if (char.chara === "千奈") {
+            tinaTotal++;
+            const div = document.querySelector(`.character[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                tinaOwned++;
+            }
+        }
+    });
+
+    const tinaPercent = tinaTotal > 0 ? ((tinaOwned / tinaTotal) * 100).toFixed(1) : "0.0";
+
+    tinaStats.innerHTML = `千奈:<span class="red">${tinaTotal}</span>種中<span class="red">${tinaOwned}</span>種（ 所持率<span class="red">${tinaPercent}%</span> ）`;
+
+    // 清夏のカードだけを対象にカウント
+    let sumikaTotal = 0;
+    let sumikaOwned = 0;
+
+    characters.forEach((char, i) => {
+        if (char.chara === "清夏") {
+            sumikaTotal++;
+            const div = document.querySelector(`.character[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                sumikaOwned++;
+            }
+        }
+    });
+
+    const sumikaPercent = sumikaTotal > 0 ? ((sumikaOwned / sumikaTotal) * 100).toFixed(1) : "0.0";
+
+    sumikaStats.innerHTML = `清夏:<span class="red">${sumikaTotal}</span>種中<span class="red">${sumikaOwned}</span>種（ 所持率<span class="red">${sumikaPercent}%</span> ）`;
+
+    // 広のカードだけを対象にカウント
+    let hiroTotal = 0;
+    let hiroOwned = 0;
+
+    characters.forEach((char, i) => {
+        if (char.chara === "広") {
+            hiroTotal++;
+            const div = document.querySelector(`.character[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                hiroOwned++;
+            }
+        }
+    });
+
+    const hiroPercent = hiroTotal > 0 ? ((hiroOwned / hiroTotal) * 100).toFixed(1) : "0.0";
+
+    hiroStats.innerHTML = `広:<span class="red">${hiroTotal}</span>種中<span class="red">${hiroOwned}</span>種（ 所持率<span class="red">${hiroPercent}%</span> ）`;
+
+    // 星南のカードだけを対象にカウント
+    let senaTotal = 0;
+    let senaOwned = 0;
+
+    characters.forEach((char, i) => {
+        if (char.chara === "星南") {
+            senaTotal++;
+            const div = document.querySelector(`.character[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                senaOwned++;
+            }
+        }
+    });
+
+    const senaPercent = senaTotal > 0 ? ((senaOwned / senaTotal) * 100).toFixed(1) : "0.0";
+
+    senaStats.innerHTML = `星南:<span class="red">${senaTotal}</span>種中<span class="red">${senaOwned}</span>種（ 所持率<span class="red">${senaPercent}%</span> ）`;
+
+    // 佑芽のカードだけを対象にカウント
+    let umeTotal = 0;
+    let umeOwned = 0;
+
+    characters.forEach((char, i) => {
+        if (char.chara === "佑芽") {
+            umeTotal++;
+            const div = document.querySelector(`.character[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                umeOwned++;
+            }
+        }
+    });
+
+    const umePercent = umeTotal > 0 ? ((umeOwned / umeTotal) * 100).toFixed(1) : "0.0";
+
+    umeStats.innerHTML = `佑芽:<span class="red">${umeTotal}</span>種中<span class="red">${umeOwned}</span>種（ 所持率<span class="red">${umePercent}%</span> ）`;
+
+    // 莉波のカードだけを対象にカウント
+    let rinamiTotal = 0;
+    let rinamiOwned = 0;
+
+    characters.forEach((char, i) => {
+        if (char.chara === "莉波") {
+            rinamiTotal++;
+            const div = document.querySelector(`.character[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                rinamiOwned++;
+            }
+        }
+    });
+
+    const rinamiPercent = rinamiTotal > 0 ? ((rinamiOwned / rinamiTotal) * 100).toFixed(1) : "0.0";
+
+    rinamiStats.innerHTML = `莉波:<span class="red">${rinamiTotal}</span>種中<span class="red">${rinamiOwned}</span>種（ 所持率<span class="red">${rinamiPercent}%</span> ）`;
+
+}
+
+
+function selectAll(flag) {
+    document.querySelectorAll(".character").forEach((div, index) => {
+        const isOwned = div.classList.contains("owned");
+        if (flag && !isOwned) {
+            div.classList.add("owned");
+            div.querySelector("img").src = characters[index].file;
+        } else if (!flag && isOwned) {
+            div.classList.remove("owned");
+            div.querySelector("img").src = characters[index].darkFile;
+        }
+    });
+    updateStats();
+}
+
+
+
+
+
+function shareOnTwitter() {
+    const owned = document.querySelectorAll(".character.owned").length;
+    const total = characters.length;
+    const percent = ((owned / total) * 100).toFixed(1);
+    const text = `学マスSSRキャラ所持率チェッカー\n全${total}種中${owned}種（ 所持率${percent}% ）\n`;
+    const url = location.href;
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(url)}`;
+    window.open(tweetUrl, "_blank");
+}
+
+updateStats();
---
+++ js/support-checker.js
@@ -0,0 +1,124 @@
+const sgrid = document.getElementById("supportGrid");
+const sstats = document.getElementById("sstats");
+
+supports.forEach((char, index) => {
+    const div = document.createElement("div");
+    div.className = "support";
+    div.dataset.index = index;
+    div.innerHTML = `
+    <img src="${char.darkFile}" alt="${char.name}">
+    <div class="support-name">${char.name}</div>
+  `;
+    div.addEventListener("click", () => togglesupport(div, index));
+    sgrid.appendChild(div);
+});
+
+function togglesupport(div, index) {
+    div.classList.toggle("owned");
+    const img = div.querySelector("img");
+    img.src = div.classList.contains("owned") ? supports[index].file : supports[index].darkFile;
+    updateStats();
+}
+
+function updateStats() {
+    const sstats = document.getElementById("sstats");
+    const vocalStats = document.getElementById("vocalStats");
+    const danceStats = document.getElementById("danceStats");
+    const visualStats = document.getElementById("visualStats");
+
+
+
+    const owned = document.querySelectorAll(".support.owned").length;
+    const total = supports.length;
+    const percent = ((owned / total) * 100).toFixed(1);
+
+    sstats.innerHTML = `全<span class="red">${total}</span>種中<span class="red">${owned}</span>種（ 所持率<span class="red">${percent}%</span> ）`;
+
+    // vocalのカードだけを対象にカウント
+    let vocalTotal = 0;
+    let vocalOwned = 0;
+
+    supports.forEach((char, i) => {
+        if (char.type === "vocal") {
+            vocalTotal++;
+            const div = document.querySelector(`.support[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                vocalOwned++;
+            }
+        }
+    });
+
+    const vocalPercent = vocalTotal > 0 ? ((vocalOwned / vocalTotal) * 100).toFixed(1) : "0.0";
+
+    vocalStats.innerHTML = `vocal:<span class="red">${vocalTotal}</span>種中<span class="red">${vocalOwned}</span>種（ 所持率<span class="red">${vocalPercent}%</span> ）`;
+
+    // danceのカードだけを対象にカウント
+    let danceTotal = 0;
+    let danceOwned = 0;
+
+    supports.forEach((char, i) => {
+        if (char.type === "dance") {
+            danceTotal++;
+            const div = document.querySelector(`.support[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                danceOwned++;
+            }
+        }
+    });
+
+    const dancePercent = danceTotal > 0 ? ((danceOwned / danceTotal) * 100).toFixed(1) : "0.0";
+
+    danceStats.innerHTML = `dance:<span class="red">${danceTotal}</span>種中<span class="red">${danceOwned}</span>種（ 所持率<span class="red">${dancePercent}%</span> ）`;
+
+    // visualのカードだけを対象にカウント
+    let visualTotal = 0;
+    let visualOwned = 0;
+
+    supports.forEach((char, i) => {
+        if (char.type === "visual") {
+            visualTotal++;
+            const div = document.querySelector(`.support[data-index='${i}']`);
+            if (div && div.classList.contains("owned")) {
+                visualOwned++;
+            }
+        }
+    });
+
+    const visualPercent = visualTotal > 0 ? ((visualOwned / visualTotal) * 100).toFixed(1) : "0.0";
+
+    visualStats.innerHTML = `visual:<span class="red">${visualTotal}</span>種中<span class="red">${visualOwned}</span>種（ 所持率<span class="red">${visualPercent}%</span> ）`;
+
+}
+
+
+
+
+function selectAll(flag) {
+    document.querySelectorAll(".support").forEach((div, index) => {
+        const isOwned = div.classList.contains("owned");
+        if (flag && !isOwned) {
+            div.classList.add("owned");
+            div.querySelector("img").src = supports[index].file;
+        } else if (!flag && isOwned) {
+            div.classList.remove("owned");
+            div.querySelector("img").src = supports[index].darkFile;
+        }
+    });
+    updateStats();
+}
+
+
+
+
+
+function shareOnTwitter() {
+    const owned = document.querySelectorAll(".support.owned").length;
+    const total = supports.length;
+    const percent = ((owned / total) * 100).toFixed(1);
+    const text = `学マスSSRサポカ所持率チェッカー\n全${total}種中${owned}種（ 所持率${percent}% ）\n`;
+    const url = location.href;
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(url)}`;
+    window.open(tweetUrl, "_blank");
+}
+
+updateStats();
